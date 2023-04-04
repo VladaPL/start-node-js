dc3b34e00b8261fa1efdd6777815e5557b098502
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-04-04 14:16:25 +0300

    feat: modules lesson

--- test.js
+++ test.js
@@ -2,7 +2,14 @@ const userName = 'Vlada';
 
 const sayHi = (userName) => `Hello, ${userName}!`;
 
-console.log(sayHi(userName));
+// console.log(sayHi(userName));
 
+// * Module
+// Берем глабальный объект module и записываем в него переменную:
+module.exports = {
+    userName,
+    sayHi,
+};
+// Теперь данные этого модуля можно использовать в других частях приложения.
 
 
